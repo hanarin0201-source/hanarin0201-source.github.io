dc0ae8a232c671f3dc918fc7be58c87568c9c3e8
kkokmatch <hanarin0201@gmail.com>
2026-08-19 00:01:19 +0900

    Route legacy refresh through external updater

--- app-v38.js
+++ app-v38.js
@@ -1,5 +1,6 @@
 (()=>{
 const ADMIN_V36='https://wjelumpbjklfrdjxbesj.supabase.co/functions/v1/kokmatch-admin-v36';
+const UPDATER='https://wjelumpbjklfrdjxbesj.supabase.co/functions/v1/kokmatch-updater';
 const renderSettings37=renderSettings;
 renderSettings=function(){
   renderSettings37();
@@ -17,7 +18,7 @@ forceUpdateApp=async function(){
       const x=await r.json().catch(()=>({}));
       if(!r.ok)throw new Error(x.error||'전체 이용자 최신화에 실패했습니다.');
     }
-    location.replace('/refresh.html?from=v38&t='+Date.now());
+    location.replace(UPDATER+'?from=v38&t='+Date.now());
   }catch(e){
     if(b){b.disabled=false;b.textContent='↻ 최신 버전으로 새로고침'}
     showError(e)
