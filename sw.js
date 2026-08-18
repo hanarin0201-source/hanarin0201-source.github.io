c5c4a9357d5fe484582a2cc15db7a43f8a666352
hanarin0201-source <hanarin0201@gmail.com>
2026-08-18 13:02:54 +0900

    Refresh Kokmatch PWA cache for check-in update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kokmatch-v2';
+const CACHE='kokmatch-v3';
 const ASSETS=['/','/index.html','/manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
