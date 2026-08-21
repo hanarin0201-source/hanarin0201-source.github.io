27491e45c48ad979ecca7f0e4c6e566369a776a9
kkokmatch <hanarin0201@gmail.com>
2026-08-21 09:59:12 +0900

    set v1.3 recovery service worker network-first

--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
+const CACHE='kokmatch-v1.3-recovery';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)))}catch{}await self.clients.claim()})())});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))) });
